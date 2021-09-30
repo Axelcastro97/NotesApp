43c2481d36f149171ccb3561bcd0c7860a3cc99f
api-veterinaria <56139828+Axelcastro97@users.noreply.github.com>
2021-09-30 14:49:25 -0300

    add cambios

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const router = express.Router();
 
-
+router.get('/notes/add', (req, res) => {
+    res.render('notes/new-note');
+});
 
 router.get('/notes', (req, res) => {
     res.end('Notes from database');
 });
 
-module.exports = router
+router.post('/notes/new-note', (req, res) => {
+    const { title, description } = req.body;
+    const errors = [];
+    if (!title) {
+        errors.push({ text: 'Por favor escriba el titulo' });
+    }
+    if (!description) {
+        errors.push({ text: 'Por favor escriba una descripción' })
+    }
+    if (errors.length > 0) {
+        res.render('notes/new-note', {
+            errors,
+            title,
+            description
+        });
+    } else {
+        res.send('ok');
+    }
+});
+
+module.exports = router;
